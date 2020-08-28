27c031eda69f4773005318ac596e6dcbb4682ec4
Kim-Chan Tze Kuo Wyz <wkimchan@avodaq.com>
2020-08-29 00:14:27 +0800

    Deleted environments files and added environments_example with a README.

---
+++ AngularRandomQuizApp/environments_example/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  CLIENT_ID: 'insert_the_client_id_from_your_oauth_registered_app_here',
+};
---
+++ AngularRandomQuizApp/environments_example/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  CLIENT_ID: 'insert_the_client_id_from_your_oauth_registered_app_here'
+};
