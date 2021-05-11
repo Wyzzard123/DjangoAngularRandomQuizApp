27390c3fe830d8da802dab746e95f964e43fff4c
sdannewitz <sdannewitz@avodaq.com>
2021-05-11 16:12:48 +0200

    environment.prod.ts now has hardcoded host value

--- AngularRandomQuizApp/src/environments/environment.prod.ts
+++ AngularRandomQuizApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   CLIENT_ID: 'insert_the_client_id_from_your_oauth_registered_app_here',
-  API_URL: 'http://localhost:4200'
+  API_URL: 'http://10.96.128.11:4200'
 };
