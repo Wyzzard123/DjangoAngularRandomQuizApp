a9ed4ff04dd785c8dc1ef8828be3ef81101da57b
Kim-Chan Tze Kuo Wyz <wkimchan@avodaq.com>
2021-05-12 11:11:03 +0800

    Fix run_django.sh user creation before database migration

--- DjangoRandomQuiz/run_django.sh
+++ DjangoRandomQuiz/run_django.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python3 manage.py shell -c "exec(\"from django.contrib.auth import get_user_model\\nif not get_user_model().objects.filter(username='admin'):\\n get_user_model().objects.create_superuser('admin', 'admin@avodaq.com', 'Cisco1234*')\")"
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
+python3 manage.py shell -c "exec(\"from django.contrib.auth import get_user_model\\nif not get_user_model().objects.filter(username='admin'):\\n get_user_model().objects.create_superuser('admin', 'admin@avodaq.com', 'Cisco1234*')\")"
 gunicorn -w 4 -b 0.0.0.0:8000 DjangoRandomQuiz.wsgi:application
